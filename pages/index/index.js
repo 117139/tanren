da5dcd295228b2a3fb21e8f07dbb4d4846969c70
unknown <unknown@example.com>
2019-07-16 10:30:03 +0800

    首页修改
    添加日期和地址

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,19 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var bmap = require('../../libs/bmap/bmap-wx.min.js');
+ var wxMarkerData = [];  //定位成功回调对象
 var pageState = require('../../utils/pageState/index.js')
+const util = require('../../utils/util.js')
 Page({
   data: {
+		ak:"HHnGRVsuG6aDpQQQveZnpOE14sk1ZyLd", //填写申请到的ak  
+    markers: [],  
+    longitude:'',   //经度  
+    latitude:'',    //纬度  
+    address:'',     //地址  
+    cityInfo:{},    //城市信息 
+		NewDate:[],    //时间
     bannerimg: [
       '/static/images/banner_03.jpg',
       '/static/images/banner_03.jpg',
@@ -15,6 +25,8 @@ Page({
     duration: 1000
   },
   onLoad: function () {
+		this.getdizhi()
+		this.gettime()
 		this.getshoplist(0)
   },
   /**
@@ -101,5 +113,43 @@ Page({
       current: current, // 当前显示图片的http链接  
       urls: arr1 // 需要预览的图片http链接列表  
     })
-  }
+  },
+	getdizhi(){
+		var that = this;  
+    /* 获取定位地理位置 */  
+    // 新建bmap对象   
+    var BMap = new bmap.BMapWX({   
+        ak: that.data.ak   
+    });   
+    var fail = function(data) {   
+        console.log(data);  
+    };   
+    var success = function(data) {   
+        //返回数据内，已经包含经纬度  
+        console.log(data);  
+        //使用wxMarkerData获取数据  
+        wxMarkerData = data.wxMarkerData;    
+        //把所有数据放在初始化data内  
+        that.setData({   
+            markers: wxMarkerData,  
+            latitude: wxMarkerData[0].latitude,  
+            longitude: wxMarkerData[0].longitude,  
+            address: wxMarkerData[0].address,  
+            cityInfo: data.originalData.result.addressComponent  
+        });   
+    }   
+    // 发起regeocoding检索请求   
+    BMap.regeocoding({   
+        fail: fail,   
+        success: success  
+    });
+	},
+	gettime(){
+		let time = util.formatTime(new Date());
+		let date=util.getDates(7, time);
+		console.log(date);
+		this.setData({
+			NewDate:date
+		})
+	}
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,60 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+
+
+function getDates(days, todate) {
+  var dateArry = [];
+  for (var i = 0; i < days; i++) {
+    var dateObj = dateLater(todate, i);
+    dateArry.push(dateObj)
+  }
+  return dateArry;
+}
+function dateLater(dates, later) {
+  let dateObj = {};
+  // let show_day = new Array('周日', '周一', '周二', '周三', '周四', '周五', '周六');
+  let show_day = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+  let date = new Date(dates);
+  date.setDate(date.getDate() + later);
+  let day = date.getDay();
+  let yearDate = date.getFullYear();
+  let month = ((date.getMonth() + 1) < 10 ? ("0" + (date.getMonth() + 1)) : date.getMonth() + 1);
+  let dayFormate = (date.getDate() < 10 ? ("0" + date.getDate()) : date.getDate());
+  dateObj.time =  yearDate+'-'+ month + '-' + dayFormate;
+	if(month=='01'){
+		dateObj.month='January';
+	}else if(month=='02'){
+		dateObj.month='February';
+	}else if(month=='03'){
+		dateObj.month='March';
+	}else if(month=='04'){
+		dateObj.month='April';
+	}else if(month=='05'){
+		dateObj.month='May';
+	}else if(month=='06'){
+		dateObj.month='June';
+	}else if(month=='07'){
+		dateObj.month='July';
+	}else if(month=='08'){
+		dateObj.month='August';
+	}else if(month=='09'){
+		dateObj.month='September';
+	}else if(month=='10'){
+		dateObj.month='October';
+	}else if(month=='11'){
+		dateObj.month='November';
+	}else if(month=='12'){
+		dateObj.month='December';
+	}
+	dateObj.day=dayFormate
+  dateObj.week = show_day[day];
+  return dateObj;
+}
+
+
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+	getDates: getDates
 }
